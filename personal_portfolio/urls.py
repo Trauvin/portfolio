830e8f35c01631c9b90a13f193f79c73bd248618
Trauvin <mateusgalote@gmail.com>
2021-06-20 16:18:53 -0300

    finished portfolio app

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello_world.urls')),
+    path("projects/", include("projects.urls")),
 ]
